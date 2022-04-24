8f9f735ae05e69aaf4d087610142506dbfd92ccd
Aadarsh Kashyap <aadarshkashyap51@gmail.com>
2022-04-24 16:07:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+document.getElementById("rock").onclick = rockPlayed;
+document.getElementById("paper").onclick = paperPlayed;
+document.getElementById("scissors").onclick = scissorsPlayed;
+
+const userScore = 0;
+const botScore = 0;
+
+function rockPlayed() {
+  play("rock");
+}
+function paperPlayed() {
+  play("paper");
+}
+function scissorsPlayed() {
+  play("scissors");
+}
+
+function play(userPlay) {
+  botPlay = getbotPlay();
+
+  document.getElementById("message").innerHTML =
+    "<p>You played <strong>" +
+    userPlay +
+    "</strong>. The bot played <strong>" +
+    botPlay +
+    "</strong>.</p>";
+
+  if (userPlay == "rock") {
+    if (botPlay == "rock") {
+      document.getElementById("message").innerHTML += "<p>You tied. :|</p>";
+    } else if (botPlay == "paper") {
+      document.getElementById("message").innerHTML += "<p>You lose. :(</p>";
+      botScore++;
+    } else if (botPlay == "scissors") {
+      document.getElementById("message").innerHTML += "<p>You win! :)</p>";
+      userScore++;
+    }
+  } else if (userPlay == "paper") {
+    if (botPlay == "rock") {
+      document.getElementById("message").innerHTML += "<p>You win! :)</p>";
+      userScore++;
+    } else if (botPlay == "paper") {
+      document.getElementById("message").innerHTML += "<p>You tied. :|</p>";
+    } else if (botPlay == "scissors") {
+      document.getElementById("message").innerHTML += "<p>You lose. :(</p>";
+      botScore++;
+    }
+  } else if (userPlay == "scissors") {
+    if (botPlay == "rock") {
+      document.getElementById("message").innerHTML += "<p>You lose. :(</p>";
+      botScore++;
+    } else if (botPlay == "paper") {
+      document.getElementById("message").innerHTML += "<p>You win! :)</p>";
+      userScore++;
+    } else if (botPlay == "scissors") {
+      document.getElementById("message").innerHTML += "<p>You tied. :|</p>";
+    }
+  }
+
+  document.getElementById("userScore").innerHTML = userScore;
+  document.getElementById("botScore").innerHTML = botScore;
+}
+
+function getbotPlay() {
+  var plays = ["rock", "paper", "scissors"];
+  var play = plays[Math.floor(Math.random() * plays.length)];
+  return play;
+}
